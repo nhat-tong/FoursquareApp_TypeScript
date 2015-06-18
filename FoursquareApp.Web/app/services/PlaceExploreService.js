c7490b3639795a7c2bd84c665bc1adfe5594f16f
Hoang Quoc Nhat TONG <thqnhat@gmail.Com>
2015-06-18 12:26:10 +0200

    convert PlaceExploreService to typescript

--- FoursquareApp.Web/app/services/PlaceExploreService.js
+++ FoursquareApp.Web/app/services/PlaceExploreService.js
@@ -4,36 +4,30 @@
 var PlaceExploreService = (function () {
     function PlaceExploreService($resource) {
         this.$resource = $resource;
-        // Request credentials of Foursquare API
-        this.requestParams = {
-            clientId: "DO5JJHGXBODWHZUZ2W45T0S35PKJH3MCLC1SKF5U4X3VF4YA",
-            clientSecret: "GF0PDCNGEKSU2GI4ANGBGBKTEUU0G3E3QYPO5YWFXRV33GY5",
-            version: "20131230"
-        };
-        this.requestUri = 'https://api.foursquare.com/v2/venues/:action';
-        this.getAction = {
-            url: this.requestUri,
-            params: { action: 'explore',
-                client_id: this.requestParams.clientId,
-                client_secret: this.requestParams.clientSecret,
-                v: this.requestParams.version,
-                venuePhotos: '1',
-                callback: 'JSON_CALLBACK' },
-            method: 'JSONP'
-        };
     }
+    // don't create instance of PlaceExploreService
+    // return data of Foursquare API using $resource
     PlaceExploreService.Factory = function ($resource) {
-        return $resource(this.prototype.requestUri, {
+        return $resource(PlaceExploreService.requestUri, {
             action: 'explore',
-            client_id: this.prototype.requestParams.clientId,
-            client_secret: this.prototype.requestParams.clientSecret,
-            v: this.prototype.requestParams.version,
+            client_id: PlaceExploreService.requestParams.clientId,
+            client_secret: PlaceExploreService.requestParams.clientSecret,
+            v: PlaceExploreService.requestParams.version,
             venuePhotos: '1',
             callback: 'JSON_CALLBACK'
         }, {
             get: { method: 'JSONP' }
         });
     };
+    // Attention with static variables
+    // Call these variables using class name: PlaceExploreService
+    // It doesn't work with this keyworks
+    PlaceExploreService.requestParams = {
+        clientId: "DO5JJHGXBODWHZUZ2W45T0S35PKJH3MCLC1SKF5U4X3VF4YA",
+        clientSecret: "GF0PDCNGEKSU2GI4ANGBGBKTEUU0G3E3QYPO5YWFXRV33GY5",
+        version: "20131230"
+    };
+    PlaceExploreService.requestUri = 'https://api.foursquare.com/v2/venues/:action';
     PlaceExploreService.$inject = ['$resource'];
     return PlaceExploreService;
 })();
--- FoursquareApp.Web/app/services/PlaceExploreService.ts
+++ FoursquareApp.Web/app/services/PlaceExploreService.ts
@@ -4,38 +4,31 @@
 "use strict";
 
 class PlaceExploreService {
-    // Request credentials of Foursquare API
-    public requestParams = {
+
+    // Attention with static variables
+    // Call these variables using class name: PlaceExploreService
+    // It doesn't work with this keyworks
+    static requestParams: any = {
         clientId: "DO5JJHGXBODWHZUZ2W45T0S35PKJH3MCLC1SKF5U4X3VF4YA",
         clientSecret: "GF0PDCNGEKSU2GI4ANGBGBKTEUU0G3E3QYPO5YWFXRV33GY5",
         version: "20131230"
     }
-
-    public requestUri: string = 'https://api.foursquare.com/v2/venues/:action';
+    static requestUri: string = 'https://api.foursquare.com/v2/venues/:action';
 
     static $inject = ['$resource'];
 
     constructor(private $resource: ng.resource.IResourceService) {
     }
 
-    public getAction: ng.resource.IActionDescriptor = {
-        url: this.requestUri,
-        params: {        action: 'explore',
-        client_id: this.requestParams.clientId,
-        client_secret: this.requestParams.clientSecret,
-        v: this.requestParams.version,
-        venuePhotos: '1',
-        callback: 'JSON_CALLBACK'},
-        method: 'JSONP'
-    }
-
-    static Factory($resource: ng.resource.IResourceService) {
-        return $resource(this.prototype.requestUri,
+    // don't create instance of PlaceExploreService
+    // return data of Foursquare API using $resource
+    public static Factory($resource: ng.resource.IResourceService) {
+        return $resource(PlaceExploreService.requestUri,
             {
                 action: 'explore',
-                client_id: this.prototype.requestParams.clientId,
-                client_secret: this.prototype.requestParams.clientSecret,
-                v: this.prototype.requestParams.version,
+                client_id: PlaceExploreService.requestParams.clientId,
+                client_secret: PlaceExploreService.requestParams.clientSecret,
+                v: PlaceExploreService.requestParams.version,
                 venuePhotos: '1',
                 callback: 'JSON_CALLBACK'
             },

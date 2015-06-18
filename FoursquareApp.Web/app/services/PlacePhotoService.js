7a8cb8f53a7607673f70c415c5cb483a959ce11a
Hoang Quoc Nhat TONG <thqnhat@gmail.Com>
2015-06-18 12:37:21 +0200

    convert PlacePhotoService to typescript

--- FoursquareApp.Web/app/services/PlacePhotoService.js
+++ FoursquareApp.Web/app/services/PlacePhotoService.js
@@ -1,28 +1,30 @@
-﻿// Request credentials of Foursquare API
+/// <reference path="../../Scripts/typings/angularjs/angular.d.ts" />
+/// <reference path="../../scripts/typings/angularjs/angular-resource.d.ts" />
 "use strict";
-
-var requestParams = {
-    clientId: "DO5JJHGXBODWHZUZ2W45T0S35PKJH3MCLC1SKF5U4X3VF4YA",
-    clientSecret: "GF0PDCNGEKSU2GI4ANGBGBKTEUU0G3E3QYPO5YWFXRV33GY5",
-    version: "20131230"
-}
-
-// Create PlacePhotos
-app.factory('PlacePhotoService', ['$resource', function ($resource) {
-    // Url
-    var requestUri = 'https://api.foursquare.com/v2/venues/:venueId/:action';
-
-    return $resource(requestUri,
-{
-    action: 'photos',
-    client_id: requestParams.clientId,
-    client_secret: requestParams.clientSecret,
-    v: requestParams.version,
-    limit: '9',
-    callback: 'JSON_CALLBACK'
-},
-{
-    get: { method: 'JSONP' }
-});
-
-}]);
+var PlacePhotoService = (function () {
+    function PlacePhotoService($resource) {
+        this.$resource = $resource;
+    }
+    PlacePhotoService.Factory = function ($resource) {
+        return $resource(PlacePhotoService.requestUri, {
+            action: 'photos',
+            client_id: PlacePhotoService.requestParams.clientId,
+            client_secret: PlacePhotoService.requestParams.clientSecret,
+            v: PlacePhotoService.requestParams.version,
+            limit: '9',
+            callback: 'JSON_CALLBACK'
+        }, {
+            get: { method: 'JSONP' }
+        });
+    };
+    PlacePhotoService.requestParams = {
+        clientId: "DO5JJHGXBODWHZUZ2W45T0S35PKJH3MCLC1SKF5U4X3VF4YA",
+        clientSecret: "GF0PDCNGEKSU2GI4ANGBGBKTEUU0G3E3QYPO5YWFXRV33GY5",
+        version: "20131230"
+    };
+    PlacePhotoService.requestUri = 'https://api.foursquare.com/v2/venues/:venueId/:action';
+    PlacePhotoService.$inject = ['$resource'];
+    return PlacePhotoService;
+})();
+angular.module('FoursquareApp').factory('PlacePhotoService', PlacePhotoService.Factory);
+//# sourceMappingURL=PlacePhotoService.js.map
---
+++ FoursquareApp.Web/app/services/PlacePhotoService.ts
@@ -0,0 +1,37 @@
+﻿/// <reference path="../../Scripts/typings/angularjs/angular.d.ts" />
+/// <reference path="../../scripts/typings/angularjs/angular-resource.d.ts" />
+
+"use strict"; 
+
+class PlacePhotoService {
+
+    private static requestParams: any = {
+        clientId: "DO5JJHGXBODWHZUZ2W45T0S35PKJH3MCLC1SKF5U4X3VF4YA",
+        clientSecret: "GF0PDCNGEKSU2GI4ANGBGBKTEUU0G3E3QYPO5YWFXRV33GY5",
+        version: "20131230"
+    };
+
+    private static requestUri: string = 'https://api.foursquare.com/v2/venues/:venueId/:action';
+
+    static $inject = ['$resource'];
+
+    constructor(private $resource: ng.resource.IResourceService) {
+    }
+
+    public static Factory($resource: ng.resource.IResourceService) {
+        return $resource(PlacePhotoService.requestUri,
+            {
+                action: 'photos',
+                client_id: PlacePhotoService.requestParams.clientId,
+                client_secret: PlacePhotoService.requestParams.clientSecret,
+                v: PlacePhotoService.requestParams.version,
+                limit: '9',
+                callback: 'JSON_CALLBACK'
+            },
+            {
+                get: { method: 'JSONP' }
+            });
+    }
+}
+
+angular.module('FoursquareApp').factory('PlacePhotoService', PlacePhotoService.Factory);
